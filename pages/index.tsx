22cacd700a9f39aab879613f148105f298a6944a
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-03-29 12:46:31 +0530

    Setup Completed

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Harsh Portfolio</title>
+        <meta name="description" content="Namaste 🙏, Welcome to my portfolio, I'm Harshvardhan Singh jadon from Gwalior, Madhya Pradesh. " />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main >
+      </main>
+    </>
+  )
+}
